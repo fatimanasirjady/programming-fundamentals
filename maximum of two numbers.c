2624d32f2b94d02096f2899707d97e82481713d1
fatimanasirjady <fatiimaa.nasir@gmail.com>
2025-09-28 01:59:24 +0500

    Add files via upload

---
+++ maximum of two numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b;
+	printf("Enter a:");
+	scanf("%d",&a);
+	printf("Enter b:");
+	scanf("%d",&b);
+	if (a==b){
+		printf("a is equals to b.");
+	}
+	else if (a>b){
+		printf("a is greater than b.");
+	}
+	else
+	{printf("b is greater than a.");
+	}
+	return 0;
+}
