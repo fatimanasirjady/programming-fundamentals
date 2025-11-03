f3d75977e75bccb21f04667438216e8fe4a112f7
fatimanasirjady <fatiimaa.nasir@gmail.com>
2025-11-03 22:30:59 +0500

    Add files via upload

---
+++ cubeintx.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int cube(int x) {
+    return x * x * x;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Cube of %d is %d\n", num, cube(num));
+    return 0;
+}
---
+++ printCourseInfo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void printCourseInfo() {
+    printf("Course: Programming Fundamentals – Learn, Code, Grow!\n");
+}
+
+int main() {
+    printCourseInfo();
+    return 0;
+}
