3441d5317f9a28c637a096cc7bfe7729420e05df
fatimanasirjady <fatiimaa.nasir@gmail.com>
2025-11-18 23:59:47 +0500

    reupload

---
+++ LABS/LAB 9/LAB9TASK2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int y = 5;
+    int z = 6;
+    int const *yPtr = &y;
+
+    printf("yPtr points to address: %p\n", yPtr);
+
+    yPtr = &z;
+    printf("Now yPtr points to address: %p\n", yPtr);
+
+    return 0;
+}
